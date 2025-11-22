ff0c0f64f544adb8a0de81b43da5982730ca283b
KevinMwita7 <kevinmwita7@gmail.com>
2025-11-22 11:02:11 +0400

    Misc improvements (#16)
    * Add main method in Picture
    
    * Use Microsoft JDK
    
    * Replace duplicated tests with single parameterized one
    
    ---------
    
    Co-authored-by: kevinmwita <kbabu@e-nsurepol.africa>

--- src/main/java/com/eyespot/imageparser/Picture.java
+++ src/main/java/com/eyespot/imageparser/Picture.java
@@ -609,4 +609,11 @@ public class Picture implements ActionListener {
       }
     }
   }
+
+  public static void main(String[] args) throws IOException {
+      Picture picture = new Picture(Path.of(args[0]));
+      String message = String.format("%d-by-%d%n", picture.width, picture.height);
+      LOGGER.log(Level.INFO, message);
+      picture.show();
+  }
 }
